896a9c5c1d3a94dfc0583b7700e679f7fdc4c64f
PWAckerman <patrickackerman@gmail.com>
2016-02-04 16:37:41 -0500

    slideout menu for mobile

--- public/js/controllers/navCtrl.js
+++ public/js/controllers/navCtrl.js
@@ -1,16 +1,24 @@
 angular.module('portfolioApp').controller('navController', ['$state', '$scope', function($state, $scope){
   var vm = $scope;
+  vm.menuState = true;
+  vm.menuToggle = function(){
+    vm.menuState = !vm.menuState
+  }
   vm.goProjects = function(){
     $state.go('projects')
+    vm.menuToggle()
   }
   vm.goEducation = function(){
     $state.go('education')
+    vm.menuToggle()
   }
   vm.goWritings = function(){
     $state.go('writings')
+    vm.menuToggle()
   }
   vm.goContact = function(){
     $state.go('contact')
+    vm.menuToggle()
   }
   vm.goHome = function(){
     $state.go('main')
