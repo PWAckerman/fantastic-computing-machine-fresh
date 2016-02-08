d5a5481ddd54d547f008300b8a5c7dfb3b082ddd
PWAckerman <patrickackerman@gmail.com>
2016-02-08 17:43:06 -0500

    background change

--- public/js/controllers/backgroundCtrl.js
+++ public/js/controllers/backgroundCtrl.js
@@ -1,13 +1,13 @@
 angular.module('portfolioApp').controller('backgroundCtrl', ['$scope', '$interval', function($scope, $interval){
   var vm = $scope;
-  vm.backgroundClass = "body-background";
-  vm.kickoff = function() {
-    vm.backgroundClass = "body-background2";
-    var switched = 0;
-    $interval(function() {
-      ++switched
-      vm.backgroundClass === "body-background2" ? vm.backgroundClass = "body-background" : vm.backgroundClass = "body-background2"
-    }, 100000)
-  }
-  vm.kickoff();
+  // vm.backgroundClass = "body-background";
+  // vm.kickoff = function() {
+  //   vm.backgroundClass = "body-background2";
+  //   var switched = 0;
+  //   $interval(function() {
+  //     ++switched
+  //     vm.backgroundClass === "body-background2" ? vm.backgroundClass = "body-background" : vm.backgroundClass = "body-background2"
+  //   }, 100000)
+  // }
+  // vm.kickoff();
 }])
