cfeab2d968c15a7bc8a61a513a6f647fec975dc4
PWAckerman <patrickackerman@gmail.com>
2016-02-11 16:17:00 -0500

    tweaks to details page

--- config/redis.js
+++ config/redis.js
@@ -1,10 +1,18 @@
 'use strict';
 let redis = require('redis'),
-redisUrl = process.env.REDIS_URL || 'redis://127.0.0.1:6379',
-client = redis.createClient(redisUrl)
+  redisUrl = process.env.REDIS_URL || 'redis://127.0.0.1:6379',
+  client = redis.createClient(redisUrl)
 
 client.on("ready", ()=>{
-    console.log(`Redis is connected, using Redis ${client.server_info.redis_version}`)
+    console.log(`Redis is connected, using Redis ${client.server_info.redis_version} at ${redisUrl}`)
 });
+
+// client.monitor((err, res)=>{
+//     console.log("Entering monitoring mode.");
+// });
+//
+// client.on("monitor", (time, args)=>{
+//     // console.log(time + ": " + util.inspect(args));
+// });
 
 module.exports = client
