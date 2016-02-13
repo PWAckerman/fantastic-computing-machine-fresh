5fe5792df7f3d1f2edaa0b9f012e886c47dba333
PWAckerman <patrickackerman@gmail.com>
2016-02-13 17:43:46 -0500

    changed background from photo to gradient

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,39 @@
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var templateCache = require('gulp-angular-templatecache');
-var paths = ['./public/partials/*.html'];
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    mocha = require('gulp-mocha'),
+    gutil = require('gulp-util'),
+    templateCache = require('gulp-angular-templatecache'),
+    paths = ['./public/partials/*.html'];
 
-gulp.task('sass', function () {
+gulp.task('sass', () => {
   gulp.src('./styles/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./public/css'));
 });
 
-gulp.task('sass:watch', function () {
+gulp.task('mocha', () => {
+  return gulp.src(['./test/**/*.js'])
+    .pipe(mocha({reporter: 'list'}))
+    .on('error', gutil.log)
+});
+
+gulp.task('mocha:watch', () => {
+  gulp.watch(['./**/*.js', './test/**/*.js'], 'mocha')
+})
+
+gulp.task('sass:watch', () => {
   gulp.watch('./styles/**/*.scss', ['sass']);
 });
 
-gulp.task('cache:watch', function () {
+gulp.task('cache:watch', () => {
   gulp.watch('./public/**/*.html', ['createTemplateCache']);
 });
 
-gulp.task('default', ['sass', 'sass:watch', 'createTemplateCache', 'cache:watch'])
+gulp.task('default', ['sass', 'sass:watch', 'createTemplateCache', 'cache:watch', 'mocha'])
 
-gulp.task('createTemplateCache', function () {
+gulp.task('createTemplateCache', () => {
     return gulp.src(paths)
         .pipe(templateCache('templates.js', { module: 'portfolioApp', root:''}))
         .pipe(gulp.dest('./public/js'));
