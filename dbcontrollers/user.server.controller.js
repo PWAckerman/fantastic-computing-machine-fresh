6c75a3087b313e3b273636dd0ed0c0f88f1a5cca
PWAckerman <patrickackerman@gmail.com>
2016-02-22 18:43:50 -0500

    features for updating site with ionic app

--- dbcontrollers/user.server.controller.js
+++ dbcontrollers/user.server.controller.js
@@ -14,7 +14,7 @@ exports.getUser = (req, res) => {
     } else {
       User.findById(req.params.id).deepPopulate(['projects', 'projects.technologies', 'projects.platforms', 'learnings', 'learnings.skill', 'skills', 'education', 'education.institution']).exec().then(
         (result) => {
-          redis.setex(result._id, 3600, JSON.stringify(result))
+          redis.setex(result._id, 10, JSON.stringify(result))
           dfd.resolve(result);
         }
       )
